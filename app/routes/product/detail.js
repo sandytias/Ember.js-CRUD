9b7e01c7bdb8d5c5cca333ed5fd47a67fd68b725
sandy <sandytiassasmita48@gmail.com>
2022-01-06 16:27:27 +0700

    erorr di bagian edit.js

--- app/routes/product/detail.js
+++ app/routes/product/detail.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class ProductDetailRoute extends Route {}
+export default class ProductDetailRoute extends Route {
+  model() {
+    let result = {
+      id: 1,
+      name: 'Siemens M-55',
+      description: 'Handphone polyphonic',
+      purchasePrice: 1500000,
+      sellPrice: 1750000,
+      isPolyphonic: true,
+    };
+    return result;
+  }
+}
--- app/routes/product/edit.js
+++ app/routes/product/edit.js
@@ -1,3 +1,16 @@
 import Route from '@ember/routing/route';
 
-export default class ProductEditRoute extends Route {}
+export default class ProductEditRoute extends Route {
+  templateName: 'product/create';
+  model() {
+    let result = {
+      id: 1,
+      name: 'Siemens M-55',
+      description: 'Handphone polyphonic',
+      purchasePrice: 1500000,
+      sellPrice: 1750000,
+      isPolyphonic: true,
+    };
+    return result;
+  }
+}
